3109b9aa40d23af071643a98117f826a7755dce7
Sabrina Baiata <sabrina@Sabrinas-Air.homenet.telecomitalia.it>
2018-06-16 16:19:02 +0200

    fine inserimento dati

--- js/main.js
+++ js/main.js
@@ -17,17 +17,34 @@ $(function(){
                 let divimmagine = $('<div></div>').addClass("col-md-12 text-center");
                 let immagine = $('<img></img>').addClass("img-fluid rounded-circle").attr("src",utente.picture.medium);
 
-                let divnome = $('<div></div>').addClass("col-md-12 text-center");
-                let nome = $('<h2></h2>').html(`${utente.name.title.charAt(0).toUpperCase()}${utente.name.title.slice(1).toLowerCase()}
+                let nomecompleto = `${utente.name.title.charAt(0).toUpperCase()}${utente.name.title.slice(1).toLowerCase()}               
                 ${utente.name.first.charAt(0).toUpperCase()}${utente.name.first.slice(1).toLowerCase()}
-                ${utente.name.last.charAt(0).toUpperCase()}${utente.name.last.slice(1).toLowerCase()}`);
+                ${utente.name.last.charAt(0).toUpperCase()}${utente.name.last.slice(1).toLowerCase()}`;
+
+                let divnome = $('<div></div>').addClass("col-md-12 text-center");
+                let nome = $('<h4></h4>').html(nomecompleto);
 
                 let divicona1 = $('<div></div>').addClass("col-md-4 text-center");
                 let divicona2 = $('<div></div>').addClass("col-md-4 text-center");
                 let divicona3 = $('<div></div>').addClass("col-md-4 text-center");
-                let icona1 = $('<i></i>').addClass("fas fa-map-marker-alt");
-                let icona2 = $('<i></i>').addClass("fas fa-mobile-alt");
-                let icona3 = $('<i></i>').addClass("fas fa-envelope");
+
+                let icona1 = $('<i></i>').addClass("fas fa-map-marker-alt").hover(function () {
+                    nome.html(`${utente.location.street}${utente.location.city}`);
+                }).mouseleave(function(){
+                    nome.html(nomecompleto);
+                });
+
+                let icona2 = $('<i></i>').addClass("fas fa-mobile-alt").hover(function(){
+                    nome.html(`${utente.phone}`);
+                }).mouseleave(function(){
+                    nome.html(nomecompleto);
+                });
+                let icona3 = $('<i></i>').addClass("fas fa-envelope").hover(function () {
+                    nome.html(`${utente.email}`);
+                }).mouseleave(function () {
+                    nome.html(nomecompleto);
+                });
+
 
                 immagine.appendTo(divimmagine);
                 divimmagine.appendTo(row1);
