12d06686360cbc28d650b4a5ec5e4f66aef63775
Sabrina Baiata <sabrina@Sabrinas-Air.homenet.telecomitalia.it>
2018-06-16 12:32:32 +0200

    aggiunta Icone

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@ $(function(){
             let lista_utenti=$("#lista_utenti");
             utenti.forEach(utente => {
                 let contenitore= $('<div></div>').addClass("col-md-4 mt-3");
+                let card= $('<div></div>').addClass("mt-3 utente p-5");
                 let row1= $('<div></div>').addClass("row");
                 let row2 = $('<div></div>').addClass("row");
                 let row3 = $('<div></div>').addClass("row");
+
                 let divimmagine = $('<div></div>').addClass("col-md-12 text-center");
                 let immagine = $('<img></img>').addClass("img-fluid rounded-circle").attr("src",utente.picture.medium);
 
@@ -20,16 +22,33 @@ $(function(){
                 ${utente.name.first.charAt(0).toUpperCase()}${utente.name.first.slice(1).toLowerCase()}
                 ${utente.name.last.charAt(0).toUpperCase()}${utente.name.last.slice(1).toLowerCase()}`);
 
+                let divicona1 = $('<div></div>').addClass("col-md-4 text-center");
+                let divicona2 = $('<div></div>').addClass("col-md-4 text-center");
+                let divicona3 = $('<div></div>').addClass("col-md-4 text-center");
+                let icona1 = $('<i></i>').addClass("fas fa-map-marker-alt");
+                let icona2 = $('<i></i>').addClass("fas fa-mobile-alt");
+                let icona3 = $('<i></i>').addClass("fas fa-envelope");
+
                 immagine.appendTo(divimmagine);
                 divimmagine.appendTo(row1);
-                row1.appendTo(contenitore);
+                row1.appendTo(card);
                 contenitore.appendTo(lista_utenti);
+                
 
                 nome.appendTo(divnome);
                 divnome.appendTo(row2);
-                row2.appendTo(contenitore);
+                row2.appendTo(card);
 
+                icona1.appendTo(divicona1);
+                divicona1.appendTo(row3); 
 
+                icona2.appendTo(divicona2);
+                divicona2.appendTo(row3);
+
+                icona3.appendTo(divicona3);
+                divicona3.appendTo(row3);
+                row3.appendTo(card);
+                card.appendTo(contenitore);
 
 
             })
